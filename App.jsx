005887075d9a56e65215a11adf773bb4f9131341
priyasambu <pd20041450@gmail.com>
2025-12-16 14:24:29 +0530

    Update and rename App.jsx to LoginApp.jsx

--- App.jsx
+++
@@ -1,7 +0,0 @@
-import Poll from "./FullStackProject/Poll";
-
-function App() {
-  return <Poll />;
-}
-
-export default App;
---
+++ LoginApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginFormValidation from "./LoginFormValidation";
+
+function App() {
+  return (
+    <div>
+      <LoginFormValidation />
+    </div>
+  );
+}
+
+export default App;
